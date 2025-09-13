c6be342b738c77fbe6a95a9607fb23da7eaaadff
Jack Maloney <jmmaloney4@gmail.com>
2025-09-13 11:39:18 -0500

    feat: add renovate.json and use jackpkgs modules

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
     jackpkgs = {
       url = "github:jmmaloney4/jackpkgs";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.just-flake.follows = "just-flake";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.systems.follows = "systems";
     };
 
     ### Flake / Project Inputs ###
@@ -15,26 +15,7 @@
       url = "github:hercules-ci/flake-parts";
     };
 
-    flake-root = {
-      url = "github:srid/flake-root";
-    };
-
-    just-flake = {
-      url = "github:juspay/just-flake";
-    };
-
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      # inputs.flake-utils.inputs.systems.follows = "systems";
-    };
-
     systems.url = "github:nix-systems/default";
-
-    treefmt = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
@@ -42,11 +23,7 @@
     nixpkgs,
     jackpkgs,
     flake-parts,
-    flake-root,
-    just-flake,
-    pre-commit-hooks,
     systems,
-    treefmt,
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} ({
       withSystem,
@@ -55,10 +32,7 @@
     }: {
       systems = import systems;
       imports = [
-        flake-root.flakeModule
-        just-flake.flakeModule
-        pre-commit-hooks.flakeModule
-        treefmt.flakeModule
+        jackpkgs.flakeModule
       ];
 
       perSystem = {
@@ -72,38 +46,12 @@
       }: {
         devShells.default = pkgs.mkShell {
           inputsFrom = [
-            config.just-flake.outputs.devShell
-            config.pre-commit.devShell
-            config.treefmt.build.devShell
+            config.jackpkgs.outputs.devShell
           ];
           buildInputs = with pkgs; [
-            fluxcd
-            kubectl
-            sops
+            just
           ];
-
-          # Add comma alias for backwards compatibility
-          shellHook = ''
-            # Set up comma alias for just
-            alias ','='just'
-            echo ""
-            echo "⚡ Alias ',' set to 'just' ✨"
-            echo ""
-          '';
         };
-
-        pre-commit = {
-          check.enable = true;
-          settings.hooks.treefmt.enable = true;
-          settings.settings.treefmt.package = config.treefmt.build.wrapper;
-        };
-
-        treefmt.config = {
-          inherit (config.flake-root) projectRootFile;
-          package = pkgs.treefmt;
-          programs.alejandra.enable = true;
-        };
-        formatter = config.treefmt.build.wrapper;
       };
     });
 }
